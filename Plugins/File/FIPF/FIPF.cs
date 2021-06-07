45f7d9dedaedc3f3f02a08d95202f4c811f76d57
Andrei Maga <andreimariomaga@gmail.com>
2021-06-07 19:00:44 +0300

    Added dialogs for selecting file and output dir

--- Plugins/File/FIPF/FIPF.cs
+++ Plugins/File/FIPF/FIPF.cs
@@ -7,9 +7,13 @@ namespace FIPF
 {
     public class FIPF : IPlugin
     {
-        private VoidCallback backCallback;
         public static Logger Logger;
+        private VoidCallback mBackCallback;
+        private Window mWindow;
+        private Toplevel mTop;
 
+        private string mFilePath;
+        private string mOutputPath;
         public string GetCategory() => "File";
 
         public string GetDescription() => "A file carving algorithm.";
@@ -23,23 +27,60 @@ namespace FIPF
         public void OnEnter(Toplevel top)
         {
             Logger.Instance.Information("Entered FIPF");
+            mTop = top;
 
-            top.RemoveAll();
-            var window = new Window("FIPF")
+            mWindow = new Window("FIPF")
             {
                 X = 0,
                 Y = 0,
                 Width = Dim.Fill(),
                 Height = Dim.Fill()
             };
-            top.Add(window);
-            top.KeyDown += KeyDownHandler;
+
+            var fileDialog = OpenDialog("Open a file", CanChooseDirectories:false);
+            var dirDialog = OpenDialog("Select a directory for the output.", CanChooseFiles: false);
+            
+            if(fileDialog == null || dirDialog == null)
+            {
+                return;
+            }
+            mFilePath = fileDialog.FilePath.ToString();
+            mOutputPath = dirDialog.DirectoryPath.ToString();
+            Logger.Information(string.Format("Using Input File {0}", mFilePath));
+            Logger.Information(string.Format("Using Output Directory {0}", mOutputPath));
+
+            mTop.RemoveAll();
+            mTop.Add(mWindow);
+            mWindow.KeyDown += KeyDownHandler;
+
             Application.Run();
+        }
+
+        public OpenDialog OpenDialog(string message, bool CanChooseFiles = true, bool CanChooseDirectories = true, bool AllowsMultipleSelection = false)
+        {
+            mWindow.RemoveAll();
+            var dialog = new OpenDialog("Open", message)
+            {
+                CanChooseFiles = CanChooseFiles,
+                CanChooseDirectories = CanChooseDirectories,
+                AllowsMultipleSelection = AllowsMultipleSelection
+            };
+
+            dialog.KeyDown += KeyDownHandler;
+
+            Application.Run(dialog);
+
+            if (dialog.Canceled)
+            {
+                mBackCallback();
+                return null;
+            }
+            return dialog;
         }
 
         public void RegisterOnBack(VoidCallback oe)
         {
-            backCallback = oe;
+            mBackCallback = oe;
         }
 
         public void KeyDownHandler(View.KeyEventEventArgs e)
@@ -52,7 +93,7 @@ namespace FIPF
 
         public void OnBack()
         {
-            backCallback();
+            mBackCallback();
         }
     }
 }
